929d983985780e7a6875444095db1cff6e53efc0
Zhifei Dong <zfdong@berkeley.edu>
2024-03-06 21:42:46 -0500

    add home and clustering page

--- app.py
+++ app.py
@@ -2,6 +2,17 @@ import streamlit as st
 from streamlit.components.v1 import html
 
 def main():
+    st.set_page_config(page_title="Overpowered", page_icon="", layout = "centered")
+    st.title('Overpowered - Connecting Renewable Energy to the Grid Faster')
+    app_choice_2 = st.selectbox('Choose Page to Navigate To:', ['Home', 'Clustering', 'Power Grid Map'])
+    if app_choice_2 == 'HOME':
+        main1()
+    elif app_choice_2 == 'Clustering':
+        main2()
+    elif app_choice_2 == 'Power Grid Map':
+        main3()
+    
+def main3():
     st.title('ArcGIS Online Map in Streamlit')
 
     # Example ArcGIS Online map URL
